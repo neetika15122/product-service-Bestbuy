250ee770c53775ba63a952d9bd0dc2b22d0d4ad8
pras0044 <prashan@algonquincollege.com>
2024-12-12 19:22:12 -0500

    save

--- src/data.rs
+++ src/data.rs
@@ -6,21 +6,21 @@ pub fn fetch_products(_settings: &Settings) -> Vec<Product> {
         Product {
             id: 1,
             name: "iphone 14Pro Max".to_string(),
-            price: 9.99,
+            price: 1570.00,
             description: "Apple's iPhone is renowned for its sleek design, powerful performance, and robust ecosystem. It features advanced camera systems, smooth iOS integration, and a wide range of applications, making it a top choice for both personal and professional use.".to_string(),
             image: "/bestbuymobile-phone.png".to_string()
         },
         Product {
             id: 2,
             name: "Airpods".to_string(),
-            price: 6.99,
+            price: 500.00,
             description: "Apple AirPods are wireless Bluetooth earbuds designed for seamless integration with Apple devices, featuring high-quality audio, instant connectivity, and voice-activated Siri access. They are known for their comfort, battery life, and the convenience of the charging case that comes with them.".to_string(),
             image: "/bestbuymobile-earphones.png".to_string()
         },
         Product {
             id: 3,
             name: "Laptop".to_string(),
-            price: 12.99,
+            price: 2100.00,
             description: "The HP Spectre series features sleek, high-performance laptops with stunning displays and powerful processors, ideal for professionals and creatives who demand efficiency and style. They typically offer long battery life, robust security features, and a lightweight design for superb portability.".to_string(),
             image: "/bestbuy-laptop.png".to_string()
         },
